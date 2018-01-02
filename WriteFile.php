f16e7aac61bc602213848d8761e2f845fb039d73
Andres Orejuela <31371194+blueleus@users.noreply.github.com>
2018-01-02 15:26:38 -0500

    Second Commit

--- WriteFile.php
+++ WriteFile.php
@@ -9,19 +9,24 @@
  * Si se ejecuta por terminal se tiene en cuenta solo el primer parametro
  * pasado.
  */
-if (isset($argv[1])) 
+if (isset($argv[1]))
 {
   /**
    * Implementacion fopen, fwrite y fclose
    * @var [type]
    */
   $file = fopen("archivo.log", "a");
-  fwrite($file, date('c').' --> '.$argv[1] . PHP_EOL);
+  fwrite($file, date('c').' --> '. $argv[1] . PHP_EOL);
   fclose($file);
 
   /**
    * Implementacion file_put_contents
    */
-  file_put_contents("archivo.log", "Otra implementacion" . PHP_EOL, FILE_APPEND | LOCK_EX);
+  file_put_contents("archivo.log", date('c') .' --> '. "Otra implementacion " . $argv[1] . PHP_EOL, FILE_APPEND | LOCK_EX);
+
+  /**
+   * Implementacion file_put_contents y print_r
+   */
+  file_put_contents("archivo.log", date('c') .' --> '. print_r($argv[1], true) . PHP_EOL, FILE_APPEND | LOCK_EX);
 }
 
